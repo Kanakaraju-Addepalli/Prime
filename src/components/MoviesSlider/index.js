032de2fdce14898fa76ff1da745b50bea88f9e9b
Kanakaraju-Addepalli <kanakaraju.ad@gmail.com>
2023-05-19 05:11:39 +0000

    First Commit

---
+++ src/components/MoviesSlider/index.js
@@ -0,0 +1,43 @@
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import MovieItem from '../MovieItem'
+
+const MovieSlider = props => {
+  const settings = {
+    dots: false,
+    slidesToScroll: 1,
+    slidesToShow: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  }
+  const {moviesList} = props
+  return (
+    <>
+      <Slider {...settings}>
+        {moviesList.map(eachMovie => (
+          <MovieItem key={eachMovie.id} movieDetails={eachMovie} />
+        ))}
+      </Slider>
+    </>
+  )
+}
+export default MovieSlider
---
+++ src/components/PrimeVideo/index.js
@@ -0,0 +1,31 @@
+import './index.css'
+import MovieSlider from '../MoviesSlider'
+
+const actionMovie = 'ACTION'
+const comedyMovie = 'COMEDY'
+
+const PrimeVideo = props => {
+  const {moviesList} = props
+  const actionMoviesList = moviesList.filter(
+    movie => movie.categoryId === actionMovie,
+  )
+  const comedyMoviesList = moviesList.filter(
+    movie => movie.categoryId === comedyMovie,
+  )
+  return (
+    <div className="prime-video-container">
+      <img
+        className="prime-image"
+        src="https://assets.ccbp.in/frontend/react-js/prime-video-img.png"
+        alt="prime video"
+      />
+      <div className="movies-container">
+        <h1 className="movies-heading">Action Movies</h1>
+        <MovieSlider moviesList={actionMoviesList} />
+        <h1 className="movies-heading">Comedy Movies</h1>
+        <MovieSlider moviesList={comedyMoviesList} />
+      </div>
+    </div>
+  )
+}
+export default PrimeVideo
